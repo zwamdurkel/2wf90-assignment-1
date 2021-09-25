2e920d091441c5aa10484f5c67a4eb9f1c520b7c
TMannens <thomasmannens@yahoo.nl>
2021-09-25 15:51:11 +0200

    hallo

--- main.py
+++ main.py
@@ -25,24 +25,19 @@ ans_loc = base_location + 'output.ops'
 ###### Using an exercise list file ######
 
 # Compile specification
-spec = asn.compile_files(ops_loc, codec="jer")
+spec = asn.compile_files(ops_loc, codec = "jer")
 
-# Read exercise list
-# open binary file
-exercise_file = open(exs_loc, 'rb')
-# read byte array
-file_data = exercise_file.read()
-# decode after specification
-my_exercises = spec.decode('Exercises', file_data)
-exercise_file.close()
+# Read exercise list 
+exercise_file = open(exs_loc, 'rb')                                # open binary file
+file_data = exercise_file.read()                                   # read byte array
+my_exercises = spec.decode('Exercises', file_data)                 # decode after specification
+exercise_file.close()                                              
 
 # Create answer JSON
 my_answers = {'exercises': []}
 
-
+# Will convert a number to an array of digits. first element = radix^0, 2nd = radix^1, etc
 def numberToArray(number, base):
-    # Will convert a number to an array of digits. first element = radix^0, 2nd = radix^1, etc
-
     array = []
 
     for i in range(len(number)):
@@ -54,9 +49,8 @@ def numberToArray(number, base):
     return array
 
 
+# Will convert array to number as string with 0-9a-f as digits
 def arrayToNumber(array):
-    # Will convert array to number as string with 0-9a-f as digits
-
     number = ''
 
     # Convert digit to proper string representation
@@ -71,9 +65,8 @@ def arrayToNumber(array):
 
     return number
 
-
+# Adds numbers in array representation with variable base
 def arrayAdd(x, y, base):
-    # Adds numbers in array representation with variable base
 
     negativeX = False
     negativeY = False
@@ -103,7 +96,7 @@ def arrayAdd(x, y, base):
             answer = arraySub(x, y, base)
             answer.append('-')
             return answer
-        else:
+        else: 
             answer = arraySub(y, x, base)
             return answer
 
@@ -113,8 +106,8 @@ def arrayAdd(x, y, base):
             answer = arraySub(y, x, base)
             answer.append('-')
             return answer
-        else:
-            answer = arraySub(x, y, base)
+        else: 
+            answer = arraySub(x, y, base) 
             return answer
 
     # Else...
@@ -125,7 +118,7 @@ def arrayAdd(x, y, base):
 
     # Do digit wise addition (and carry)
     for i in range(size):
-        answer.append(x[i] + y[i] + carry)
+        answer.append( x[i] + y[i] + carry )
         carry = 0
 
         if answer[i] >= base:
@@ -141,9 +134,7 @@ def arrayAdd(x, y, base):
 
     return answer
 
-
 def arraySub(x, y, base):
-    # Subtracts numbers in array representation with variable base
 
     negativeX = False
     negativeY = False
@@ -155,7 +146,7 @@ def arraySub(x, y, base):
         y.pop()
         negativeY = True
 
-    # Get max size to fill rest with zeros
+    # Get max size to fill rest with zeros 
     size = max(len(x), len(y))
     # Extend numbers with trailing 0 (doesn't change value)
     y += [0] * (size - len(y))
@@ -189,26 +180,25 @@ def arraySub(x, y, base):
         return answer
 
     # Else...
-
+    
     # Prepare empty carry and answer array
     answer = []
     carry = 0
-
+       
     for i in range(size):
-        answer.append(x[i] - y[i] - carry)
+        answer.append( x[i] - y[i] - carry )
         carry = 0
 
         if answer[i] < 0:
             answer[i] += base
             carry = 1
-
+    
     # Remove trailing 0
     while answer[-1] == 0 and answer != [0]:
         answer.pop()
-
+    
     return answer
 
-<<<<<<< HEAD
 def arrayMult(x, y, base):
 
     negativeX = False
@@ -252,18 +242,14 @@ def arrayMult(x, y, base):
         answer = arrayAdd(answerArray[i], answer, base)
 
     return answer
-=======
->>>>>>> d74795916c6d7becd3b4ed5660f26935a262f803
 
 # Loop over exercises and solve
 for exercise in my_exercises['exercises']:
-    # get operation type
-    operation = exercise[0]
-    # get parameters
-    params = exercise[1]
+    operation = exercise[0]                                        # get operation type
+    params = exercise[1]                                           # get parameters
 
     print(f'Running exercise:\n{exercise}')
-
+    
     if operation == 'add':
         ### Do addition ###
 
@@ -273,7 +259,7 @@ for exercise in my_exercises['exercises']:
 
         params['answer'] = arrayToNumber(arrayAdd(x, y, params['radix']))
         print(f"Answer: {params['answer']}")
-
+    
     if operation == 'subtract':
         ### Do subtraction ###
 
@@ -286,20 +272,23 @@ for exercise in my_exercises['exercises']:
 
     if operation == 'multiply':
         ### Do multiplication ###
-        params['answer'] = '66'
-        params['count-mul'] = '1'
-        params['count-add'] = '2'
+        # Convert to array to handle more easily
+        x = numberToArray(params['x'], params['radix'])
+        y = numberToArray(params['y'], params['radix'])
 
+        params['answer'] = arrayToNumber(arrayMult(x, y, params['radix']))
+        print(f"Answer: {params['answer']}")
+    
     if operation == 'mod-add':
         ### Do modular addition ###
         params['answer'] = '1234'
-
+    
     if operation == 'euclid':
         ### Do euclidean algorithm ###
         params['answ-d'] = '1'
         params['answ-a'] = '0'
         params['answ-b'] = '0'
-
+    
     # etc.
 
     # Save answer
@@ -308,8 +297,6 @@ for exercise in my_exercises['exercises']:
 ###### Creating an answers list file ######
 
 # Save exercises with answers to file
-# write to binary file
-my_file = open(ans_loc, 'wb+')
-# add encoded exercise list
-my_file.write(json.dumps(my_answers).encode())
+my_file = open(ans_loc, 'wb+')                                       # write to binary file
+my_file.write(json.dumps(my_answers).encode())                       # add encoded exercise list
 my_file.close()
